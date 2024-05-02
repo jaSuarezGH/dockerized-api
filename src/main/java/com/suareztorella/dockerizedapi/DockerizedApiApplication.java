5fcf71f46f5a8c7eb3eea7f9b41e996aaa278a46
José Suárez <74939658+jaSuarezGH@users.noreply.github.com>
2024-05-02 13:19:32 -0400

    Initial commit

---
+++ src/main/java/com/suareztorella/dockerizedapi/DockerizedApiApplication.java
@@ -0,0 +1,13 @@
+package com.suareztorella.dockerizedapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerizedApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerizedApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suareztorella/dockerizedapi/DockerizedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suareztorella.dockerizedapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerizedApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
