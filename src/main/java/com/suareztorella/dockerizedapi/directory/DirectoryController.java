6ed4c4f97d40c0043d13a9901e66ce3f3631cccf
Angelo Torella <82620221+AngeloTorella@users.noreply.github.com>
2024-05-08 18:46:38 -0400

    Update DirectoryController.java
    arreglos en el endpoint patch id

--- src/main/java/com/suareztorella/dockerizedapi/directory/DirectoryController.java
+++ src/main/java/com/suareztorella/dockerizedapi/directory/DirectoryController.java
@@ -2,6 +2,7 @@ package com.suareztorella.dockerizedapi.directory;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -61,21 +62,23 @@ public class DirectoryController {
 
     }
 
+    @SuppressWarnings("unchecked")
     @PatchMapping(path = "/{id}")
-    public Object modifyPartiallyDirectory(@PathVariable("id") Long id, @RequestBody Directory directory) {
-
+    public Object modifyPartiallyDirectory(@PathVariable("id") Long id, @RequestBody Map<String, Object> jsonData) {
         try {
-            directoryRepository.findById(id).get();
+            Directory directory = directoryRepository.findById(id).get();
 
-            // Declare the new modified directory
-            Directory moded_directory = new Directory();
-            moded_directory.setId(id);
-            moded_directory.setName(directory.getName());
-            moded_directory.setEmails(directory.getEmails());
+            // Actualizar los datos del directorio con los valores del JSON
+            if (jsonData.containsKey("name")) {
+                directory.setName((String) jsonData.get("name"));
+            }
+            if (jsonData.containsKey("emails")) {
+                directory.setEmails((List<String>) jsonData.get("emails"));
+            }
 
-            return directoryRepository.save(moded_directory);
+            return directoryRepository.save(directory);
         } catch (NoSuchElementException e) {
-            return "No se encontro directorio con id " + id;
+            return "No se encontró directorio con id " + id;
         }
     }
 
